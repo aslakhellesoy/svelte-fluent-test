1a92a5b3f4d9f4f51ab27e37676835b6f0d348a9
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2023-02-23 23:21:17 +0000

    Add svelte-fluent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { FluentBundle, FluentResource } from '@fluent/bundle';
+	import { FluentProvider, Localized } from '@nubolab-ffwd/svelte-fluent';
+
+	const translations = 'hello = Hello, world!';
+	const bundle = new FluentBundle('en');
+	bundle.addResource(new FluentResource(translations));
+</script>
+
+<FluentProvider bundles={[bundle]}>
+	<Localized id="hello" />
+</FluentProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import svelteFluent from '@nubolab-ffwd/svelte-fluent/rollup-plugin';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), svelteFluent()]
 });
